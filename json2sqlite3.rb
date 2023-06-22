b753bcc89a813ad005797049339865a53569f9b6
Yamashita, Yuu <peek824545201@gmail.com>
2023-06-22 10:30:39 +0900

    Updated brew's formula; 0.20230622.1

--- json2sqlite3.rb
+++ json2sqlite3.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Json2sqlite3 < Formula
-  VERSION = '0.20230608.1'
+  VERSION = '0.20230622.1'
   homepage 'https://github.com/yyuu/json2sqlite3'
   url 'https://github.com/yyuu/json2sqlite3.git', tag: "v#{VERSION}"
   head 'https://github.com/yyuu/json2sqlite3.git', branch: "main"
