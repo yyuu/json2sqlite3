0b8e9cd6bc01c0e3f37369b0c686ac2a32172d9a
Yamashita, Yuu <peek824545201@gmail.com>
2024-02-24 21:52:43 +0900

    Added `bats-core` as a test dependency

--- json2sqlite3.rb
+++ json2sqlite3.rb
@@ -7,6 +7,7 @@ class Json2sqlite3 < Formula
   head 'https://github.com/yyuu/json2sqlite3.git', branch: "main"
 
   depends_on "bash"
+  depends_on "bats-core" => [:build, :test]
   depends_on "coreutils" => :recommended
   depends_on "jq"
   depends_on "sqlite" => :recommended
